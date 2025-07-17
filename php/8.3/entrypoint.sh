f771747395e05e9fc3ab02acf03c8d3e3a825e1a
aspirantzhang <admin@aspirantzhang.com>
2025-07-17 19:22:24 +0800

    feat: enhance entrypoint script to use custom Nginx configuration if available

--- php/8.3/entrypoint.sh
+++ php/8.3/entrypoint.sh
@@ -1,7 +1,11 @@
 #!/bin/sh
 set -e
 
-envsubst '$NGINX_ROOT' < /etc/nginx/conf.d/default.conf.template > /etc/nginx/conf.d/default.conf
+if [ -f /etc/nginx/custom.conf ]; then
+    cp /etc/nginx/custom.conf /etc/nginx/conf.d/default.conf
+else
+    envsubst '$NGINX_ROOT' < /etc/nginx/conf.d/default.conf.template > /etc/nginx/conf.d/default.conf
+fi
 
 if [ "${VITE_ENABLED}" = "true" ]; then
     envsubst '$PROJECT_ROOT' < /etc/supervisor/conf.d/vite.conf.template >> /etc/supervisor/conf.d/supervisord.conf
